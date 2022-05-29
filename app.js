b547d81669dfb5df9c1e1e873853026c3d8f1132
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-05-29 06:44:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-var myArr = [];
-let currentPage = window.location.href;
+ myArr = [
+   "https://www.instagram.com/",
+   "https://www.youtube.com/",
+   "https://www.facebook.com/",
+   "https://www.reddit.com/",
+ ];
+
+let counter = 0;
 
 document.getElementById("myButton").addEventListener("click", pushData);
 
-function pushData()
-{
-    var inputText = document.getElementById("inputText").value;
+function pushData() {
+  var inputText = document.getElementById("inputText").value;
+  myArr.push(inputText);
 
-    myArr.push(inputText);
-    console.log(myArr);
+  console.log(myArr);
 }
 
-pushData()
-console.write(currentPage);
+pushData();
+
+chrome.tabs.query(
+  {
+    active: true,
+    lastFocusedWindow: true,
+  },
+  function (tabs) {
+    // and use that tab to fill in out title and url
+    var tab = tabs[0];
+    function checkArray() {
+      for (i = 0; i < myArr.length; i++) {
+        if (tab.url == myArr[i]) {
+            console.log("ohhhhh you in troble mah boy");
+            counter++;
+            console.log(counter);
+            window.alert("YOU'VE BREACHED THE TRUST "+ counter +" TIMES" );
+        } else {
+          console.log("u chillen");
+        }
+      }
+    }
+    checkArray();
+    console.log(tab.url);
+  }
+);
